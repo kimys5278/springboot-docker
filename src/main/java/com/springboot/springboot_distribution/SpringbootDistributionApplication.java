62aa7b912468536074cc31edfd95dcae60cbfedc
kimys5278 <kimys5278@gmail.com>
2024-05-29 21:35:42 +0900

    feat: project build

---
+++ src/main/java/com/springboot/springboot_distribution/SpringbootDistributionApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.springboot_distribution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDistributionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDistributionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/springboot_distribution/SpringbootDistributionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.springboot_distribution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDistributionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
